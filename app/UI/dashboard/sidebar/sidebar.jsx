d0f33605bcf8e3053db26ae461881dcda824d88d
Damitha Perera <damithap@99x.io>
2023-12-31 13:46:18 +0530

    side bar

--- app/UI/dashboard/sidebar/sidebar.jsx
+++ app/UI/dashboard/sidebar/sidebar.jsx
@@ -1,7 +1,89 @@
+import styles from "./sidebar.module.css";
+import {
+    MdDashboard,
+    MdSupervisedUserCircle,
+    MdShoppingBag,
+    MdAttachMoney,
+    MdWork,
+    MdAnalytics,
+    MdPeople,
+    MdOutlineSettings,
+    MdHelpCenter,
+    MdLogout,
+} from "react-icons/md";
+
+const menuItems = [
+    {
+        title: "Pages",
+        list: [
+            {
+                title: "Dashboard",
+                path: "/dashboard",
+                icon: <MdDashboard />,
+            },
+            {
+                title: "Users",
+                path: "/dashboard/users",
+                icon: <MdSupervisedUserCircle />,
+            },
+            {
+                title: "Products",
+                path: "/dashboard/products",
+                icon: <MdShoppingBag />,
+            },
+            {
+                title: "Transactions",
+                path: "/dashboard/transactions",
+                icon: <MdAttachMoney />,
+            },
+        ],
+    },
+    {
+        title: "Analytics",
+        list: [
+            {
+                title: "Revenue",
+                path: "/dashboard/revenue",
+                icon: <MdWork />,
+            },
+            {
+                title: "Reports",
+                path: "/dashboard/reports",
+                icon: <MdAnalytics />,
+            },
+            {
+                title: "Teams",
+                path: "/dashboard/teams",
+                icon: <MdPeople />,
+            },
+        ],
+    },
+    {
+        title: "User",
+        list: [
+            {
+                title: "Settings",
+                path: "/dashboard/settings",
+                icon: <MdOutlineSettings />,
+            },
+            {
+                title: "Help",
+                path: "/dashboard/help",
+                icon: <MdHelpCenter />,
+            },
+        ],
+    },
+];
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div className={styles.container}>
+            <ui>
+                {menuItems.map((cat) => (
+                    <li key={cat.title}>{cat.title}</li>
+                ))}
+            </ui>
+        </div>
     )
 }
 
--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,13 +1,14 @@
 import Sidebar from "@/app/UI/dashboard/sidebar/sidebar";
 import Navabr from "@/app/UI/dashboard/navbar/navabr";
+import styles from "@/app/UI/dashboard/dashboard.module.css";
 
 const Layout = ({children}) => {
     return (
-        <div>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.menu}>
                 <Sidebar/>
             </div>
-            <div>
+            <div className={styles.content}>
                 <Navabr/>
                 {children}
             </div>
